6e279791869255e80491b0ccd3b904b6dad24498
DuongTrung2212 <duongtrungqb12@gmail.com>
2024-04-28 14:43:08 +0700

    update forms

--- src/app/(app)/(HeaderLayout)/(Auth)/create-post/page.tsx
+++ src/app/(app)/(HeaderLayout)/(Auth)/create-post/page.tsx
@@ -110,7 +110,7 @@ export default function CreatePostPage() {
   };
 
   return (
-    <div className="w-4/5 flex flex-col gap-y-5 py-[20px] px-[30px] m-auto bg-white mt-[20px] rounded-lg">
+    <div className="w-4/5 max-lg:w-full flex flex-col gap-y-5 py-[20px] px-[30px] max-lg:px-[15px] m-auto bg-white mt-[20px] rounded-lg">
       <Form
         form={form}
         validateMessages={{
@@ -146,7 +146,12 @@ export default function CreatePostPage() {
               <Button htmlType="submit">as</Button>
             </Form.Item>
           </Form> */}
-            <div className="w-full flex gap-x-10">
+            <div className="w-full flex gap-x-10 max-lg:flex-col">
+              <ModalCategorySelectCustom
+                className="hidden max-lg:block mb-[20px]"
+                onChangeKey={(e) => currentForm.setCurrentForm?.(e)}
+                label="Danh mục tin đăng"
+              />
               <div className="flex-1">
                 <b>Ảnh / video sản phẩm</b>
                 <Space className="flex text-[#9b9b9b] text-[13px]">
@@ -157,7 +162,7 @@ export default function CreatePostPage() {
                     </p>
                   </Link>
                 </Space>
-                <div className="w-[300px] min-h-[200px] flex items-center justify-center">
+                <div className="w-[300px] max-lg:w-full min-h-[200px] flex items-center justify-center">
                   <Dragger
                     className="truncate w-full"
                     name="images_A1_data"
@@ -190,7 +195,7 @@ export default function CreatePostPage() {
                     />
                   </Modal>
                 </div>
-                <div className="w-[300px] min-h-[200px] py-[20px] flex items-center justify-center">
+                <div className="w-[300px] max-lg:w-full min-h-[200px] py-[20px] flex items-center justify-center">
                   <Dragger
                     className="truncate w-full"
                     name="Video"
@@ -223,18 +228,11 @@ export default function CreatePostPage() {
               </div>
               <div className="flex-[3_2_0%]">
                 <ModalCategorySelectCustom
+                  className="max-lg:hidden"
                   onChangeKey={(e) => currentForm.setCurrentForm?.(e)}
                   label="Danh mục tin đăng"
                 />
                 <CreatePostBusinessPremisesForm />
-                {/* {currentForm.currentForm ? (
-              getFormByKey(currentForm.currentForm)
-            ) : (
-              <ModalCategorySelectCustom
-                onChangeKey={(e) => currentForm.setCurrentForm?.(e)}
-                label="Danh mục tin đăng"
-              />
-            )} */}
                 <Flex gap={20} className="my-[20px]">
                   <Form.Item className="flex-1">
                     <button
